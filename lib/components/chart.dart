c7d93a14afdc066630acd0a9e4f6e5cbe3f567e5
bentley <bentley@besquare.com.my>
2023-06-21 22:13:33 +0800

    Before I start functional

---
+++ lib/components/chart.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:candlesticks/candlesticks.dart';
+
+class CandleStick extends StatelessWidget{
+  const CandleStick({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Column()
+    );
+  }
+}
