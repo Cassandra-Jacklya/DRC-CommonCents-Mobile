680300417f614a285bfb91acda9cddc93a709fc9
Cassandra-Jacklya <cassandrajacklya7@gmail.com>
2023-07-05 11:44:35 +0800

    resolved conflicts

--- lib/pages/newspage.dart
+++ lib/pages/newspage.dart
@@ -174,131 +174,130 @@ class _NewsPageState extends State<NewsPage> {
                   _NewsData = snapshot.data as List<dynamic>;
                   // print("News data: ${_NewsData.length}");
                   if (_textEditingController.text.isNotEmpty) {
-                    // print("Before: ${_constantData.length}");
-                    final filteredList =
-                        _applyFilter(_NewsData, _textEditingController.text);
-                    // print("Const: ${_constantData.length}");
-                    return ListView.builder(
-                        controller: controller,
-                        itemCount: filteredList.length + 1,
-                        itemBuilder: (context, index) {
-                          if (index < filteredList.length) {
-                            final news = filteredList[index];
-                            return Container(
-                              margin: const EdgeInsets.all(10),
-                              height: 100,
-                              color: Colors.grey[300],
-                              padding: const EdgeInsets.all(10),
-                              child: Row(
-                                children: [
-                                  GestureDetector(
-                                    onTap: () async {
-                                      Uri url = Uri.parse(news['url']);
-                                      launchUrl(url);
-                                    },
-                                    child: Container(
-                                      color: Colors.white,
-                                      height: 80,
-                                      width: 80,
-                                      child: Image.network(
-                                        (news['banner_image'] != null &&
-                                                news['banner_image'] != "")
-                                            ? news['banner_image']
-                                            : 'https://static.vecteezy.com/system/resources/previews/000/440/213/original/question-mark-vector-icon.jpg',
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(width: 10),
-                                  Expanded(
-                                    child: Text(
-                                      news[
-                                          'title'], // Update this with the appropriate key for the news title
-                                      style: TextStyle(
-                                        color: Theme.of(context).primaryColor,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          }
-                        });
+                    final filteredList = _applyFilter(
+                        _OriginalNewsList, _textEditingController.text);
+                    // return ListView.builder(
+                    //     controller: controller,
+                    //     itemCount: filteredList.length + 1,
+                    //     itemBuilder: (context, index) {
+                    //       if (index < filteredList.length) {
+                    //         final news = filteredList[index];
+                    //         return Container(
+                    //           margin: const EdgeInsets.all(10),
+                    //           height: 90,
+                    //           width: 321,
+                    //           color: Colors.grey[300],
+                    //           padding: const EdgeInsets.all(10),
+                    //           child: Row(
+                    //             children: [
+                    //               GestureDetector(
+                    //                 onTap: () async {
+                    //                   print(news['url']);
+                    //                   Uri url = Uri.parse(news['url']);
+                    //                   launchUrl(url);
+                    //                 },
+                    //                 child: Container(
+                    //                   color: Colors.white,
+                    //                   height: 80,
+                    //                   width: 80,
+                    //                   child: Image.network(
+                    //                     (news['banner_image'] != null &&
+                    //                             news['banner_image'] != "")
+                    //                         ? news['banner_image']
+                    //                         : 'https://static.vecteezy.com/system/resources/previews/000/440/213/original/question-mark-vector-icon.jpg',
+                    //                     fit: BoxFit.cover,
+                    //                   ),
+                    //                 ),
+                    //               ),
+                    //               const SizedBox(width: 10),
+                    //               Expanded(
+                    //                 child: Text(
+                    //                   news[
+                    //                       'title'], // Update this with the appropriate key for the news title
+                    //                   style: TextStyle(
+                    //                     color: Theme.of(context).primaryColor,
+                    //                     fontSize: 16,
+                    //                     fontWeight: FontWeight.bold,
+                    //                   ),
+                    //                 ),
+                    //               ),
+                    //             ],
+                    //           ),
+                    //         );
+                    //       }
+                    //     });
+                    return NewsContainer(feeds: filteredList, scrollable: true,);
                   } else if (_textEditingController.text.isEmpty) {
-                    bool once = true;
-                    if (once) {
-                      final int originalNewsListLength =
-                          _OriginalNewsList.length;
-                      const int takeCount = 8;
-                      _OriginalNewsList.addAll(
-                          _NewsData.skip(originalNewsListLength)
-                              .take(takeCount));
-                      once =!once;
-                    }
-
-                    print(_OriginalNewsList.length);
-                    return ListView.builder(
-                      controller: controller,
-                      itemCount: _OriginalNewsList.length + 1,
-                      itemBuilder: (context, index) {
-                        if (index < _OriginalNewsList.length) {
-                          final news = _OriginalNewsList[index];
-                          return Container(
-                            margin: const EdgeInsets.all(10),
-                            height: 100,
-                            color: Colors.grey[300],
-                            padding: const EdgeInsets.all(10),
-                            child: Row(
-                              children: [
-                                GestureDetector(
-                                  onTap: () async {
-                                    Uri url = Uri.parse(news['url']);
-                                    launchUrl(url);
-                                  },
-                                  child: Container(
-                                    color: Colors.white,
-                                    height: 80,
-                                    width: 80,
-                                    child: Image.network(
-                                      (news['banner_image'] != null &&
-                                              news['banner_image'] != "")
-                                          ? news['banner_image']
-                                          : 'https://static.vecteezy.com/system/resources/previews/000/440/213/original/question-mark-vector-icon.jpg',
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                                const SizedBox(width: 10),
-                                Expanded(
-                                  child: Text(
-                                    news[
-                                        'title'], // Update this with the appropriate key for the news title
-                                    style: TextStyle(
-                                      color: Theme.of(context).primaryColor,
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          );
-                        } else {
-                          return isLoading
-                              ? const SizedBox(
-                                  height: 100,
-                                  width: 100,
-                                  child: CircularProgressIndicator())
-                              : Container(
-                                  color: Colors.red,
-                                  child: const Text("No News Available"),
-                                );
-                        }
-                      },
-                    );
-                  }
+                    final int originalNewsListLength = _OriginalNewsList.length;
+                    final int takeCount =
+                        originalNewsListLength < 8 ? originalNewsListLength : 8;
+                    _newsList
+                        .addAll(_OriginalNewsList.take(takeCount).toList());
+                    _OriginalNewsList.removeRange(0, takeCount);
+                    // return ListView.builder(
+                    //     controller: controller,
+                    //     itemCount: _newsList.length + 1,
+                    //     itemBuilder: (context, index) {
+                    //       if (index < _newsList.length) {
+                    //         final news = _newsList[index];
+                    //         return Container(
+                    //           margin: const EdgeInsets.all(10),
+                    //           height: 100,
+                    //           color: Colors.grey[300],
+                    //           padding: const EdgeInsets.all(10),
+                    //           child: Row(
+                    //             children: [
+                    //               GestureDetector(
+                    //                 onTap: () async {
+                    //                   print(news['url']);
+                    //                   Uri url = Uri.parse(news['url']);
+                    //                   launchUrl(url);
+                    //                 },
+                    //                 child: Container(
+                    //                   color: Colors.white,
+                    //                   height: 80,
+                    //                   width: 80,
+                    //                   child: Image.network(
+                    //                     (news['banner_image'] != null &&
+                    //                             news['banner_image'] != "")
+                    //                         ? news['banner_image']
+                    //                         : 'https://static.vecteezy.com/system/resources/previews/000/440/213/original/question-mark-vector-icon.jpg',
+                    //                     fit: BoxFit.cover,
+                    //                   ),
+                    //                 ),
+                    //               ),
+                    //               const SizedBox(width: 10),
+                    //               Expanded(
+                    //                 child: Text(
+                    //                   news[
+                    //                       'title'], // Update this with the appropriate key for the news title
+                    //                   style: TextStyle(
+                    //                     color: Theme.of(context).primaryColor,
+                    //                     fontSize: 16,
+                    //                     fontWeight: FontWeight.bold,
+                    //                   ),
+                    //                 ),
+                    //               ),
+                    //             ],
+                    //           ),
+                    //         );
+                    //       } else if (_OriginalNewsList.isEmpty) {
+                    //         return Container(
+                    //           color: Colors.red,
+                    //           child: const Text("No News Available"),
+                    //         );
+                    //       } else {
+                    //         return isLoading
+                    //             ? const SizedBox(
+                    //                 height: 100,
+                    //                 width: 100,
+                    //                 child: CircularProgressIndicator())
+                    //             : Container();
+                    //       }
+                    //     },
+                    //     );
+                    return NewsContainer(feeds: _NewsData, scrollable: true,);
+                  } 
                 }
                 return const Text('No News Available');
               },
